63b074c25c7d5bb3269be4d6088d301a0b5b15cf
Tatiana <tatianacalderon10@gmail.com>
2023-06-14 08:51:35 -0500

    feat: :tada: Se inicio proyecto sistema campus

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BASE DE DATOS CAMPUS LANDS</title>
+</head>
+<body>
+
+    <h1>HOLA MUNDO</h1>
+
+    <p>luis carlos habla mucho</p>
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <h3>Buenas</h3>
+    <ul>
+        <li>Tatis</li>
+        <li>Erik</li>
+        <li>LuisK</li>
+        <li>Yooo</li>
+    </ul>
+
+    <table>
+        <th>
+            <tr>Hola</tr>
+        </th>
+        <td>
+            <tr>MUNDO</tr>
+        </td>
+    </table>
+    
+
+    <p>no se actualiza esto </p>
+
+
+</body>
+</html>
